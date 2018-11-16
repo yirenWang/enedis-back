5b4088ad2051be544974919f1637220bc632c05b
Yiren Wang <yiren.wang@student.ecp.fr>
2018-11-16 13:45:26 +0100

    correct url to contact data

--- user.js
+++ user.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const getUserFromEnedis = (accessToken, usagePointId) => {
-  const url = `https://gw.hml.api.enedis.fr/v3/customers/identity?usage_point_id=${usagePointId}`;
+  const url = `https://gw.hml.api.enedis.fr/v3/customers/contact_data?usage_point_id=${usagePointId}`;
   const options = {
     headers: {
       Accept: 'application/json',
