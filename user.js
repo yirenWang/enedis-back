6ea84d66f6a5399598ad2cc89f65f06370616392
Yiren Wang <yiren.wang@student.ecp.fr>
2018-11-16 13:41:43 +0100

    correct url

--- user.js
+++ user.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const getUserFromEnedis = (accessToken, usagePointId) => {
-  const url = `https://gw.hml.enedis.fr/v3/customers/identity?usage_point_id=${usagePointId}`;
+  const url = `https://gw.hml.api.enedis.fr/v3/customers/identity?usage_point_id=${usagePointId}`;
   const options = {
     headers: {
       Accept: 'application/json',
